be2e612d9b28ce4cf4218e1eb3b25d25970e0e74
sutkar <adityasutkar@gmail.com>
2021-06-20 02:00:22 +0530

    3 files modified - practice js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  content=document.getElementById("mainGrid").innerHTML;
   name = 'Angular';
+  
+player=1;
+
+
+    clicked(blockNo){
+    //console.log(blockNo);
+console.log(this.content);
+  if(this.player == 1){
+document.getElementById(blockNo).className='circle center';
+this.player=2;
+  }else{
+  document.getElementById(blockNo).className='cross center';
+  this.player=1;
+}
+
+  }
+
+  reset(){
+    console.log(this.content);
+document.getElementById("mainGrid").innerHTML=this.content;
+  }
 }
